f81f16085b3289a4ae99ac140f919bf655199a9d
pawel <pawelkomar42@gmail.com>
2023-03-15 17:17:56 +0300

    3

--- src/test/java/AddItemTest.java
+++ src/test/java/AddItemTest.java
@@ -1,17 +1,15 @@
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.core.IsEqual.equalTo;
 
 public class AddItemTest {
-    //@BeforeAll
-    @Test
-    void setUp() {
+    @BeforeAll
+
+    static void setUp() {
         RestAssured.filters(new AllureRestAssured());
         given()
                 .queryParam("apiKey", "0f938cbff9194b06be6c25f0799e9c3f")
@@ -23,33 +21,39 @@ public class AddItemTest {
 
     @Test
     void addItemTest() {
-        RestAssured.filters(new AllureRestAssured());
         given()
                 .queryParam("apiKey", "0f938cbff9194b06be6c25f0799e9c3f")
                 .queryParam("includeNutrition", "true")
-                .pathParam("username", "cool-user13")
                 .queryParam("hash", "b50625841c1a1a630f2bfdc874798474f93e0ff2")
+                .pathParam("username", "cool-user13")
                 .body("{\n" +
                         "\t\"item\": \"1 package baking powder\",\n" +
                         "\t\"aisle\": \"Baking\",\n" +
-                        "\t\"parse\": false\n" +
+                        "\t\"parse\": true\n" +
                         "}")
                 .when()
                 .post("https://api.spoonacular.com/mealplanner/{username}/shopping-list/items")
-                .then().assertThat().body(equalTo("{\n" +
-                        "\t\"item\": \"1 package baking powder\",\n" +
-                        "\t\"aisle\": \"Baking\",\n" +
-                        "\t\"parse\": false\n" +
-                        "}"))
+                .then()
                 .contentType(ContentType.JSON)
                 .statusCode(200);
     }
 
-    @AfterAll
-    static void teamDown() {
+   /* @Test
+    void getShoppingListTest() {
+        given()
+                .queryParam("apiKey", "0f938cbff9194b06be6c25f0799e9c3f")
+                .queryParam("includeNutrition", "true")
+                .queryParam("hash", "b50625841c1a1a630f2bfdc874798474f93e0ff2")
+                .pathParam("username", "cool-user13")
+                .get("https://api.spoonacular.com/mealplanner/{username}/shopping-list");
+    }*/
+
+    @Test
+    void teamDown() {
         given()
                 .queryParam("hash", "b50625841c1a1a630f2bfdc874798474f93e0ff2")
                 .pathParam("username", "cool-user13")
-                .delete("https://api.spoonacular.com/mealplanner/{username}/shopping-list/items");
+                .pathParam("id", 1488393)
+                .delete("https://api.spoonacular.com/mealplanner/{username}/shopping-list/items/{id}");
     }
 }
